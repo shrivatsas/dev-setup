daf9293722f96196bba901944197aef8902ed53b
Shrivatsa <shrivatsa.dev@gmail.com>
2025-11-03 10:45:24 +0530

    Use uv instead of pyenv

--- ubuntu.sh
+++ ubuntu.sh
@@ -21,24 +21,6 @@ sudo apt install git -y
 # Install the packages required to compile Python
 sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-# Download pyenv code from github
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-
-# Define environment variable PYENV_ROOT to point to the path where pyenv repo is cloned
-echo "set --export PYENV_ROOT $HOME/.pyenv" > ~/.config/fish/conf.d/pyenv.fish
-
-# Add $PYENV_ROOT/bin to your $PATH for access to the pyenv command-line utility
-set -U fish_user_paths $HOME/.pyenv/bin $fish_user_paths
-
-# Add pyenv init to your shell to enable shims and autocompletion.
-echo -e '\n\n# pyenv init\nif command -v pyenv 1>/dev/null 2>&1\n  pyenv init - | source\nend' >> ~/.config/fish/config.fish
-
-# Install pyenv-virtualenv
-git clone https://github.com/pyenv/pyenv-virtualenv.git (pyenv root)/plugins/pyenv-virtualenv
-
-# Enable virtualenv autocomplete
-echo -e "\n# Enable virtualenv autocomplete\nstatus --is-interactive; and pyenv init - | source\nstatus --is-interactive; and pyenv virtualenv-init - | source\n" >> ~/.config/fish/conf.d/pyenv.fish
-
 # Install Visual Studio Code
 sudo apt install code
 
